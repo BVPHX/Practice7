233bf0f52b45cb37e207cee4cba45d77b5a13f83
mpiece228@gmail.com <mpiece228@gmail.com>
2021-11-16 23:24:22 +0300

    Добавьте файлы проекта.

---
+++ Practice5/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Triad;
+
+namespace Practice5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Triads firstTriad = new Triads();
+        Triads secondTriad = new Triads();
+        public MainWindow()
+        {
+            InitializeComponent();
+            firstTriadBox.Text = firstTriad.First.ToString() + " " + firstTriad.Second.ToString() + " " + firstTriad.Third.ToString();
+            secondTriadBox.Text = secondTriad.First.ToString() + " " + secondTriad.Second.ToString() + " " + secondTriad.Third.ToString();
+
+        }
+
+        private void FirstTriadIncrease(object sender, RoutedEventArgs e)
+        {
+            firstTriad.IncreaseTriadValuesTo10();
+            firstTriadBox.Text = firstTriad.First.ToString() + " " + firstTriad.Second.ToString() + " " + firstTriad.Third.ToString();
+        }
+
+        private void SecondTriadIncrease(object sender, RoutedEventArgs e)
+        {
+            secondTriad.IncreaseTriadValuesTo10();
+            secondTriadBox.Text = secondTriad.First.ToString() + " " + secondTriad.Second.ToString() + " " + secondTriad.Third.ToString();
+        }
+
+        private void CompareButton(object sender, RoutedEventArgs e)
+        {
+            if (firstTriad > secondTriad) MessageBox.Show("Значения первой триады больше значению второй");
+            else MessageBox.Show("Некоторые значения первой триады меньше значений второй триады");
+        }
+
+        private void FirstDateFill(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(firstDateDayBox.Text) < 32 && Convert.ToInt32(firstDateDayBox.Text) > 0)
+            {
+                if (Convert.ToInt32(firstDateMonthBox.Text) > 0 && Convert.ToInt32(firstDateMonthBox.Text) < 13)
+                {
+                    Date firstDate = new Date(Convert.ToInt32(firstDateDayBox.Text), Convert.ToInt32(firstDateMonthBox.Text), Convert.ToInt32(firstDateYearBox.Text));
+                    firstDateOutputBox.Text = firstDate.Day.ToString() + "." + firstDate.Month.ToString() + "." + firstDate.Year.ToString();
+                }
+
+
+            }
+            else MessageBox.Show("Дата указана некорректно");
+        }
+
+        private void SecondDatFill(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(secondDateDayBox.Text) < 32 && Convert.ToInt32(secondDateDayBox.Text) > 0)
+            {
+                if (Convert.ToInt32(secondDateMonthBox.Text) > 0 && Convert.ToInt32(secondDateMonthBox.Text) < 13)
+                {
+                    Date secondDate = new Date(Convert.ToInt32(secondDateDayBox.Text), Convert.ToInt32(secondDateMonthBox.Text), Convert.ToInt32(secondDateYearBox.Text));
+                    secondDateOutputBox.Text = secondDate.Day.ToString() + "." + secondDate.Month.ToString() + "." + secondDate.Year.ToString();
+                }
+            }
+            else MessageBox.Show("Дата указана некорректно");
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
---
+++ Triad/Triad.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Triad
+{
+    public class Triads
+    {
+        protected int _first;
+        protected int _second;
+        protected int _third;
+
+        public Triads()
+        {
+            Random rnd = new Random();
+            _first = rnd.Next(0, 10);
+            _second = rnd.Next(0, 10);
+            _third = rnd.Next(0, 10);
+        }
+
+        public int First { get { return _first; } set { _first = value; } }
+        public int Second { get { return _second; } set { _second = value; } }
+        public int Third { get { return _third; } set { _third = value; } }
+
+        public static bool operator >(Triads firstTriad, Triads secondTriad)
+        {
+            if (firstTriad.First > secondTriad.First && firstTriad.Second > secondTriad.Second && firstTriad.Third > secondTriad.Third) return true;
+            else return false;
+        }
+        public static bool operator <(Triads firstTriad, Triads secondTriad)
+        {
+            if (firstTriad.First < secondTriad.First && firstTriad.Second < secondTriad.Second && firstTriad.Third < secondTriad.Third) return true;
+            else return false;
+        }
+
+        public void IncreaseTriadValuesTo10()
+        {
+            _first += 10;
+            _second += 10;
+            _third += 10;
+        }
+    }
+    public class Date : Triads
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public Date(int day, int month, int year) : base()
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public static bool DateCompare(Date date1, Date date2)
+        {
+            if (date1.Year == date2.Year)
+            {
+                if (date1.Month >= date2.Month)
+                {
+                    if (date1.Day > date2.Day) return true;
+                }
+            }
+            else return false;
+            if (date1.Year > date2.Year) return true;
+            if (date1.Year < date2.Year) return false;
+            else return false;
+
+        }
+    }
+
+}
